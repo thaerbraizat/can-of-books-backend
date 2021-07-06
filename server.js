647186f1825561e03ce508d7b98c15088867c7ec
thaerbraizat <thaerbraizat13@gmail.com>
2021-07-06 15:34:28 +0300

    fixthing

--- server.js
+++ server.js
@@ -3,46 +3,89 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
-
+// const jwt = require('jsonwebtoken');
+// const jwksClient = require('jwks-rsa');
+const mongoose=require('mongoose');
 const app = express();
+const homeController=require('./controllers/home.controller')
+
+const bookController=require('./controllers/book.controller')
+
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT;
+
+mongoose.connect('mongodb://localhost:27017/favBooks',
+{useNewUrlParser:true,useUnifiedTopology:true})
+
+app.get('/',homeController);
+app.get('/book',bookController);
 
 
 
-const client = jwksClient({
-
-  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-});
+app.listen(port, () => console.log(`listening on ${port}`));
 
 
-const getKey = (header, callback) => {
-  client.getSigningKey(header.kid, function (err, key) {
-    const signingKey = key.publicKey || key.rsaPublicKey;
-    callback(null, signingKey);
-  });
-}
-app.get('/', (req, res) =>{
-  console.log(client);
 
-} );
 
-app.get('/test', (req, res) => {
-  console.log(req.headers.authorization);
 
-  const token = req.headers.authorization.split(' ')[1];
-  jwt.verify(token, getKey, {}, (err, user) => {
-    if (err) {
-      res.send('invalid token');
-    }
-    res.send(user)
-  })
-});
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const client = jwksClient({
+
+//   jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+// });
+
+
+// const getKey = (header, callback) => {
+//   client.getSigningKey(header.kid, function (err, key) {
+//     const signingKey = key.publicKey || key.rsaPublicKey;
+//     callback(null, signingKey);
+//   }); 
+// }
+// app.get('/', (req, res) =>{
+//   console.log(client);
+
+// } );
+
+// app.get('/test', (req, res) => {
+//   console.log(req.headers.authorization);
+
+//   const token = req.headers.authorization.split(' ')[1];
+//   jwt.verify(token, getKey, {}, (err, user) => {
+//     if (err) {
+//       res.send('invalid token');
+//     }
+//     res.send(user)
+//   })
+//   res.send(token)
+// });
+
+
+
+
+
+
+
+
+
+
 
 
 
